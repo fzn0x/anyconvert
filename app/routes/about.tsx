1b270ade0ea59474e6d0fda91f761308b2f957be
fzn0x <fncolon@pm.me>
2024-05-16 22:56:44 +0700

    fix: font size in mobile responsive

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -4,8 +4,8 @@ import type { MetaFunction } from "@remix-run/node";
 
 import { Text, Flex, Button } from "@chakra-ui/react";
 
-import Navbar from '~/components/Navbar';
-import Footer from '~/components/Footer';
+import Navbar from "~/components/Navbar";
+import Footer from "~/components/Footer";
 
 export const meta: MetaFunction = () => {
   return [
@@ -17,18 +17,34 @@ export const meta: MetaFunction = () => {
 export default function Root() {
   return (
     <>
-      <Navbar/>
-      <Flex direction="column" w="60%" m="auto" pt={20} minHeight="100vh">
+      <Navbar />
+      <Flex
+        direction="column"
+        w={["80%", "60%"]}
+        m="auto"
+        pt={20}
+        minHeight="100vh"
+      >
         <Flex direction="column" gap={10}>
-            <Text fontWeight="700" fontSize="100px">About Us</Text>
-            <Text w="100%" fontWeight="500" fontSize="30px" whiteSpace={"pre-wrap"}>
-                AnyConvert was initiated by Muhammad Fauzan and maintained by the Community, Muhammad Fauzan is the Founder of AnyConvert and CTO of SpellCode.inc. {"\n\n"}
-
-                You can use this app to track convertion rates of any listed currencies and cryptocurrencies for <b>free</b> and we will <i>never</i> accept any offers to commercialize this product.
-            </Text>
+          <Text fontWeight="700" fontSize={["50px", "100px"]}>
+            About Us
+          </Text>
+          <Text
+            w="100%"
+            fontWeight="500"
+            fontSize={["20px", "30px"]}
+            whiteSpace={"pre-wrap"}
+          >
+            AnyConvert was initiated by Muhammad Fauzan and maintained by the
+            Community, Muhammad Fauzan is the Founder of AnyConvert and CTO of
+            SpellCode.inc. {"\n\n"}
+            You can use this app to track convertion rates of any listed
+            currencies and cryptocurrencies for <b>free</b> and we will{" "}
+            <i>never</i> accept any offers to commercialize this product.
+          </Text>
         </Flex>
       </Flex>
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -4,8 +4,8 @@ import type { MetaFunction } from "@remix-run/node";
 
 import { Text, Flex, Button } from "@chakra-ui/react";
 
-import Navbar from '~/components/Navbar';
-import Footer from '~/components/Footer';
+import Navbar from "~/components/Navbar";
+import Footer from "~/components/Footer";
 
 export const meta: MetaFunction = () => {
   return [
@@ -17,20 +17,35 @@ export const meta: MetaFunction = () => {
 export default function Root() {
   return (
     <>
-      <Navbar/>
-      <Flex direction="column" w="60%" m="auto" pt={20} minHeight="100vh">
+      <Navbar />
+      <Flex
+        direction="column"
+        w={["80%", "60%"]}
+        m="auto"
+        pt={20}
+        minHeight="100vh"
+      >
         <Flex direction="column" gap={10}>
-            <Text fontWeight="700" fontSize="100px">Contact</Text>
-            <Text w="40%" noOfLines={3} fontWeight="500" fontSize="30px">developerfauzan@gmail.com</Text>
-            {/* https://www.linkedin.com/pulse/write-less-say-more-tips-succinct-engaging-email-content-minal-patel-b2zfe/ */}
-            <Text w="100%" fontWeight="500" fontSize="30px">
-                Your time is important to me; please ensure communications are concise and to the point.
-
-                For questions regarding this application, we encourage utilizing the github repository issues.
-            </Text>
+          <Text fontWeight="700" fontSize={["50px", "100px"]}>
+            Contact
+          </Text>
+          <Text
+            w={["100%", "40%"]}
+            noOfLines={3}
+            fontWeight="500"
+            fontSize={["20px", "30px"]}
+          >
+            developerfauzan@gmail.com
+          </Text>
+          {/* https://www.linkedin.com/pulse/write-less-say-more-tips-succinct-engaging-email-content-minal-patel-b2zfe/ */}
+          <Text w="100%" fontWeight="500" fontSize={["20px", "30px"]}>
+            Your time is important to me; please ensure communications are
+            concise and to the point. For questions regarding this application,
+            we encourage utilizing the github repository issues.
+          </Text>
         </Flex>
       </Flex>
-      <Footer/>
+      <Footer />
     </>
   );
 }
